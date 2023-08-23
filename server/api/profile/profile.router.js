a2b4ce3f9b8eeabb8e430a31e2ce25fa66c0c6ed
Anteneh008 <anteneh.atre@gmail.com>
2023-08-23 15:50:02 +0300

    remove cors config at profile router

--- server/api/profile/profile.router.js
+++ server/api/profile/profile.router.js
@@ -15,14 +15,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// Use CORS middleware specifically for the profile picture upload route
-const corsOptions = {
-  origin: "https://4337a2e4.forum-frontend-3fm.pages.dev", // Replace with your frontend domain
-  methods: "POST", // Allow only POST requests
-  credentials: true, // Enable cookies and authentication headers
-};
 
 // Define the route and attach the middleware and controller function
-router.post("/",cors(corsOptions), upload.single("profilePicture"), handleUploadProfilePicture);
+router.post("/", upload.single("profilePicture"), handleUploadProfilePicture);
 
 module.exports = router;
