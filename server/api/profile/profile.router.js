9a705814b56678666a08cd24e71e6b2b815a2b48
Anteneh008 <anteneh.atre@gmail.com>
2023-08-23 15:39:45 +0300

    cors config at profile router

--- server/api/profile/profile.router.js
+++ server/api/profile/profile.router.js
@@ -2,23 +2,27 @@ const router = require("express").Router();
 const multer = require("multer");
 const path = require("path");
 
-const {
-    handleUploadProfilePicture
-} = require("./profile.controller");
+const { handleUploadProfilePicture } = require("./profile.controller");
 // router.post("/", handleUploadProfilePicture);
-
 
 // Set up the multer middleware for file uploads
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "uploads"),
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    },
-  });
+  destination: path.join(__dirname, "uploads"),
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-  const upload = multer({ storage });
+const upload = multer({ storage });
 
-  // Define the route and attach the middleware and controller function
-router.post("/", upload.single("profilePicture"), handleUploadProfilePicture);
+// Use CORS middleware specifically for the profile picture upload route
+const corsOptions = {
+  origin: "https://4337a2e4.forum-frontend-3fm.pages.dev", // Replace with your frontend domain
+  methods: "POST", // Allow only POST requests
+  credentials: true, // Enable cookies and authentication headers
+};
+
+// Define the route and attach the middleware and controller function
+router.post("/",cors(corsOptions), upload.single("profilePicture"), handleUploadProfilePicture);
 
 module.exports = router;
